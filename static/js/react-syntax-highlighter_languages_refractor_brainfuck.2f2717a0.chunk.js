0bc2eabae0d9aee804a53b12c4efaafef6bff154
Jesus Villalobos <jvillalobos@berkeley.edu>
2021-07-25 20:27:04 -0700

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_brainfuck.2f2717a0.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcs-central"]=this["webpackJsonpcs-central"]||[]).push([[18],{292:function(a,e,t){"use strict";function n(a){a.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}a.exports=n,n.displayName="brainfuck",n.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.2f2717a0.chunk.js.map
